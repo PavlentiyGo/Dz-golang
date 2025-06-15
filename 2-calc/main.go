f7a86ac4e6b6720c9f0d32700afa50be2b65de48
PavlentiyGo <goryachev.p.v@edu.mirea.ru>
2025-06-15 15:05:19 +0300

    доработка функций

--- 2-calc/main.go
+++ 2-calc/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func UserScan() {
+	var operation string
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		scanner.Scan()
+		operation = scanner.Text()
+		if operation == "AVG" || operation == "SUM" || operation == "MED" {
+			break
+		} else {
+			fmt.Println("Ошибка, значение должно быть AVG, SUM, MED")
+		}
+	}
+	fmt.Println("Введите желаемые числа через запятую c пробелом")
+	for {
+		ok := scanner.Scan()
+		if !ok {
+			fmt.Println("Ошибка ввода")
+		} else {
+			break
+		}
+	}
+	text := scanner.Text()
+	numbers := strings.Split(text, ",")
+	sum := 0
+	for _, number := range numbers {
+		number_int, _ := strconv.Atoi(number)
+		sum += number_int
+	}
+	switch operation {
+	case "AVG":
+		fmt.Println(sum / len(numbers))
+	case "SUM":
+		fmt.Println(sum)
+	case "MED":
+		fmt.Println(numbers[len(numbers)/2])
+	}
+}
+
+func main() {
+	UserScan()
 }
