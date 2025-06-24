f382723dadf427926b421d6b76e3453207aa6615
PavlentiyGo <goryachev.p.v@edu.mirea.ru>
2025-06-24 21:42:05 +0300

    up 3

---
+++ 3-bin/bins/bins.go
@@ -0,0 +1,24 @@
+package bins
+
+import "time"
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+type BinList struct {
+	bins []Bin
+}
+
+func newBin(id string, private bool, name string) *Bin {
+	return &Bin{id: id,
+		private:   private,
+		createdAt: time.Now(),
+		name:      name,
+	}
+}
+func newBinList(bin *Bin) *BinList {
+	return &BinList{bins: []Bin{*bin}}
+}
